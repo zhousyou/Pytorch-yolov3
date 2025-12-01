d0e01343df847a38498f55a40308b9f02b77be59
zzy <1597463346@qq.com>
2025-12-01 22:51:25 +0800

    1. 补充VOC数据集类初始化相关信息 2. 补充处理图像以及标注的相关内容

--- data_utils.py
+++ data_utils.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import xml.etree.ElementTree as ET
 import config
+import os
 
 class VOCDataset(Dataset):
     def __init__(self, data_dir, split = 'train', img_size = 416, transform = None):
@@ -21,5 +22,65 @@ class VOCDataset(Dataset):
         self.split = split
         self.img_size = img_size
         self.transform = transform
-        self.classes = config.voc_classes
+        
+        # VOC数据集的20个类别
+        with open(config.voc_classes, 'r') as file:
+            self.classes = [line.strip() for line in file.readlines()]
+        
+        # 建立类别-索引映射字典
+        self.class_to_id = {name: idx for idx, name in enumerate(self.classes)}
+        self.num_classes = len(self.classes)
 
+        # 加载图像列表
+        self.image_ids = self._load_image_ids()
+
+        # 打印加载图像信息
+        print(f"加载VOC2007{self.split}数据集:{len(self.image_ids)}张图像,{self.num_classes}个种类")
+
+    
+    def _load_image_ids(self):
+        """
+            加载图像ID列表,返回train/val/trainval.txt中对应图像id
+        """
+
+        split_file = os.path.join(
+            self.data_dir, 'ImageSets', 'Main', f'{self.split}.txt'
+        )
+        with open(split_file, 'r') as file:
+            image_ids = [line.strip() for line in file.readlines()]
+        
+        return image_ids
+    
+    def __len__(self):
+        return len(self.image_ids)
+    
+    def __getitem__(self, index):
+
+        img_id = self.image_ids[index]
+        # --------------------------
+        # 加载图像
+        # --------------------------
+        img_path = os.path.join(
+            self.data_dir, 'JPEGImages', f'{img_id}.jpg'
+        )
+        img = cv2.imread(img_path)
+
+        # 异常处理
+        if img is None:
+            raise FileNotFoundError(f"无法加载图像: {img_path}")
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        original_h, original_w = img.shape[:2] # 图像的格式是[B, C, H, W]
+
+        # --------------------------
+        # 加载标注
+        # --------------------------
+        annotation_path = os.path.join(
+            self.data_dir, 'Annotations', f'{img_id}.xml'
+        )
+    
+    
+
+if __name__ == "__main__":
+    data_dir = 'VOCdevkit/VOC2007'
+    VOCDataset(data_dir=data_dir,
+               split='train')
