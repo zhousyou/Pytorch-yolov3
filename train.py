5041258e29cdb5c3cbcf550d83f371510662342f
zzy <1597463346@qq.com>
2025-12-08 22:19:27 +0800

    补充训练以及main()代码

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from loss import YOLOLoss
 import config
+import data_utils
+from model.yolo import YoloV3
 
 class YOLOV3_trainer:
     def __init__(self, model, train_loader, val_loader, device):
@@ -149,5 +151,74 @@ class YOLOV3_trainer:
             torch.save(checkpoint, best_path)
             print(f"保存最佳的模型,loss:{self.best_loss:.4f}")
 
+    def train(self):
+        """
+            完整的训练流程
+        """
+        print("开始训练YOLO V3 on VOC...")
+        print(f"设备{self.device}")
+        print(f"训练集大小{len(self.train_loader.dataset)}")
+        print(f"验证集大小{len(self.val_loader.dataset)}")
+
+        for epoch in range(self.cur_epoch, config.epochs):
+            self.cur_epoch = epoch
+            print(f"\nEpoch{epoch}/{config.epochs}")
+            print('-' * 50 )
+
+            # 训练
+            train_loss, train_loss_dict = self.train_epoch()
+
+            # 验证
+            val_loss, val_loss_dict = self.validation()
+
+            # 更新学习率
+            self.scheduler.step()
+
+            # 打印结果
+
+            print(f"训练损失:{train_loss:.4f}")
+            print(f"验证损失:{val_loss:.4f}")
+            print(f"训练损失分量:{train_loss_dict:.4f}")
+            print(f"验证损失分量:{val_loss_dict:.4f}")
+            print(f"学习率:{self.optimizer.param_groups[0]['lr']:.6f}")
+
+            # 保存检查点
+            is_best = val_loss < self.best_loss
+            if is_best:
+                self.best_loss = val_loss
+            
+            if epoch % config.save_interval == 0 or is_best:
+                self.save_checkpoint(is_best)
+
+
+def main():
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"使用设备:{device}")
+
+    # 创建数据加载器
+
+    train_loader, val_loader = data_utils.create_voc_dataloaders(
+        data_dir= config.data_dir,
+        batch_size= config.batch_size,
+        img_size= config.img_size
+    )
+
+    # 创建模型
+    model = YoloV3(num_classes=config.num_classes).to(device)
+
+    # 打印模型信息
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f"模型参数量:{num_params:,}")
+
+    # 创建训练器
+    trainer = YOLOV3_trainer(model, train_loader, val_loader, device)
+
+    # 开始训练
+    trainer.train()
+
+
+
+
 if __name__ == "__main__":
-    print(config.learning_rate)
+    main()
